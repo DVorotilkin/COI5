3e6fe613b895b75aaefc949e26c8df1975816a9b
Dima <Dima>
2017-11-14 18:21:37 +0500

    Done

---
+++ Lab5/Lab5/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьИзображениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.морфологическаяОбработкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.дилатацияtoolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.построениеОстоваToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.выпуклаяОболочкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox2.Location = new System.Drawing.Point(318, 37);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.морфологическаяОбработкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(732, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.загрузитьИзображениеToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // загрузитьИзображениеToolStripMenuItem
+            // 
+            this.загрузитьИзображениеToolStripMenuItem.Name = "загрузитьИзображениеToolStripMenuItem";
+            this.загрузитьИзображениеToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.загрузитьИзображениеToolStripMenuItem.Text = "Загрузить изображение";
+            this.загрузитьИзображениеToolStripMenuItem.Click += new System.EventHandler(this.загрузитьИзображениеToolStripMenuItem_Click);
+            // 
+            // морфологическаяОбработкаToolStripMenuItem
+            // 
+            this.морфологическаяОбработкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.дилатацияtoolStripMenuItem,
+            this.выпуклаяОболочкаToolStripMenuItem,
+            this.построениеОстоваToolStripMenuItem});
+            this.морфологическаяОбработкаToolStripMenuItem.Name = "морфологическаяОбработкаToolStripMenuItem";
+            this.морфологическаяОбработкаToolStripMenuItem.Size = new System.Drawing.Size(184, 20);
+            this.морфологическаяОбработкаToolStripMenuItem.Text = "Морфологическая обработка";
+            // 
+            // дилатацияtoolStripMenuItem
+            // 
+            this.дилатацияtoolStripMenuItem.Name = "дилатацияtoolStripMenuItem";
+            this.дилатацияtoolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.дилатацияtoolStripMenuItem.Text = "Дилатация";
+            this.дилатацияtoolStripMenuItem.Click += new System.EventHandler(this.дилатацияtoolStripMenuItem_Click);
+            // 
+            // построениеОстоваToolStripMenuItem
+            // 
+            this.построениеОстоваToolStripMenuItem.Name = "построениеОстоваToolStripMenuItem";
+            this.построениеОстоваToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.построениеОстоваToolStripMenuItem.Text = "Построение остова";
+            this.построениеОстоваToolStripMenuItem.Click += new System.EventHandler(this.построениеОстоваToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(624, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 13;
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(657, 56);
+            this.trackBar5.Maximum = 20;
+            this.trackBar5.Minimum = 1;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar5.Size = new System.Drawing.Size(45, 281);
+            this.trackBar5.TabIndex = 26;
+            this.trackBar5.Value = 1;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(621, 37);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(81, 13);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "Размер маски";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(636, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "Go...";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // выпуклаяОболочкаToolStripMenuItem
+            // 
+            this.выпуклаяОболочкаToolStripMenuItem.Name = "выпуклаяОболочкаToolStripMenuItem";
+            this.выпуклаяОболочкаToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.выпуклаяОболочкаToolStripMenuItem.Text = "Выпуклая оболочка";
+            this.выпуклаяОболочкаToolStripMenuItem.Click += new System.EventHandler(this.выпуклаяОболочкаToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 371);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Лаба 5";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьИзображениеToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem морфологическаяОбработкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem построениеОстоваToolStripMenuItem;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ToolStripMenuItem дилатацияtoolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem выпуклаяОболочкаToolStripMenuItem;
+    }
+}
+
---
+++ Lab5/Lab5/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap origImage;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Элементы управления
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+            label10.Text = "Размер маски = " + Convert.ToString(trackBar5.Value);
+        }
+        private void дилатацияtoolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = toBitmap(EroseImage(toArray(pictureBox1.Image),
+                                                    trackBar5.Value, 255));
+        }
+        private void построениеОстоваToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = toBitmap(Skeleton(toArray(pictureBox1.Image),
+                                                    trackBar5.Value, 255));
+        }
+
+        private void выпуклаяОболочкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = toBitmap(ConvexShell(toArray(pictureBox1.Image), 255));
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            var pos = pictureBox1.PointToClient(Cursor.Position);
+            var res = toArray(origImage);
+            FillingArea(res, pos.X, pos.Y, 100);
+            pictureBox2.Image = toBitmap(res);
+        }
+
+        private void загрузитьИзображениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Image image = Image.FromFile(dialog.FileName);
+                origImage = new Bitmap(image, pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.Image = origImage;
+                морфологическаяОбработкаToolStripMenuItem.Enabled = true;
+            }
+        }
+        #endregion
+
+        #region Вспомогательные методы
+
+        public byte[,] toArray(Image image)
+        {
+            byte[,] res = new byte[image.Width, image.Height];
+            Bitmap bmp10 = new Bitmap(image);
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    res[x, y] = (byte)(bmp10.GetPixel(x, y).GetBrightness() * 255 > 100?255:0);
+                }
+            }
+            return res;
+        }
+
+        public Bitmap toBitmap(byte[,] byteMass)
+        {
+            int width = byteMass.GetLength(0);
+            int height = byteMass.GetLength(1);
+            Bitmap res = new Bitmap(width, height);
+            for (int x = 0; x < width; ++x)
+                for (int y = 0; y < height; ++y)
+
+                {
+                    Color color = Color.FromArgb(byteMass[x, y],
+                        byteMass[x, y],
+                        byteMass[x, y]);
+                    res.SetPixel(x, y, color);
+                }
+            return res;
+        }
+        #endregion
+
+        #region Морфологическая обработка
+        private byte HM(byte[,] image, byte[,] mask, int x, int y, byte brightness)
+        {
+            for (int i = -1; i <= 1; ++i)
+                for (int j = -1; j <= 1; ++j)
+                {
+                    if (mask[i+1, j+1] == 1 && image[x + i, y + j] != brightness)
+                        return image[x, y];
+                    if (mask[i + 1, j + 1] == 0 && image[x + i, y + j] == brightness)
+                        return image[x, y];
+                }
+            return brightness;
+        }
+            
+        private byte[,] HMImage(byte[,] image, byte[,] mask, byte brightness)
+        {
+            byte[,] res = (byte[,])image.Clone();
+            bool f = true;
+            while (f)
+            {
+                f = false;
+                for (int x = 1; x < pictureBox1.Width - 1; x++)
+                    for (int y = 1; y < pictureBox1.Height - 1; y++)
+                    {
+                        byte oldVal = res[x, y];
+                        res[x, y] = HM(res, mask, x, y, brightness);
+                        if (oldVal != res[x, y])
+                            f = true;
+                    }
+            }
+
+            return res;
+        }
+
+        private byte[,] ConvexShell(byte[,] image, byte brightness)
+        {
+            byte[,] mask = { { 1, 2, 2},
+                             { 1, 0, 2},
+                             { 1, 2, 2} };
+            var res = HMImage(image, mask, brightness);
+            mask = new byte[,]{ { 1, 1, 1},
+                                { 2, 0, 2},
+                                { 2, 2, 2} };
+            res = HMImage(res, mask, brightness);
+            mask = new byte[,]{ { 2, 2, 1},
+                                { 2, 0, 1},
+                                { 2, 2, 1} };
+            res = HMImage(res, mask, brightness);
+            mask = new byte[,]{ { 2, 2, 2},
+                                { 2, 0, 2},
+                                { 1, 1, 1} };
+            return HMImage(res, mask, brightness);
+        }
+
+        private byte[,] EroseImage(byte[,] originalBright, int border, byte brightness)
+        {
+            byte[,] res = new byte[pictureBox1.Width, pictureBox1.Height];
+            for (int x = border; x < pictureBox1.Width - border; x++)
+                for (int y = border; y < pictureBox1.Height - border; y++)
+                    res[x, y] = Erose(originalBright, x, y, border, brightness);
+            return res;
+        }
+
+        private byte Erose(byte[,] image, int x, int y, int border, byte brightness)
+        {
+            for (int m = x - border / 2 - 1; m <= x + border / 2 + 1; m++)
+                for (int n = y - border / 2 - 1; n <= y + border / 2 + 1; n++)
+                    if (image[m, n] == brightness)
+                        return brightness;
+            return image[x,y];
+        }
+
+        private byte[,] OpeningClosing(byte[,] originalBright, int border)
+        {
+            return EroseImage(EroseImage(originalBright, border, 0), border, 255);
+        }
+
+        private byte[,] Skeleton(byte[,] originalBright, int border, byte brightness)
+        {
+            byte[,] erosenImg = (byte[,])originalBright.Clone();
+            byte[,] OCbright;
+            byte[,] res = new byte[pictureBox1.Width, pictureBox1.Height];
+            byte diff;
+            int flag = 1;
+            while (flag != 0)
+            {
+                flag = 0;
+                erosenImg = EroseImage(erosenImg, border, 0);
+                OCbright = OpeningClosing(erosenImg, border);
+                for (int x = border; x < pictureBox1.Width - border; x++)
+                    for (int y = border; y < pictureBox1.Height - border; y++)
+                    {
+                        diff = (byte)(erosenImg[x, y] - OCbright[x, y]);
+                        if (diff == brightness)
+                        {
+                            flag++;
+                        }
+                        res[x, y] += diff;
+                    }
+            }
+            return res;
+        }
+        
+        void FillingArea(byte[,] originalBright, int x, int y, byte brightness)
+        {
+            Queue<Point> nextPoints = new Queue<Point>();
+            HashSet<Point> processedPoints = new HashSet<Point>();
+            nextPoints.Enqueue(new Point(x, y));
+            Point currentPoint;
+            Point nextPoint;
+            byte origBrightness = originalBright[x, y];
+
+            while (nextPoints.Count != 0)
+            {
+                currentPoint = nextPoints.Dequeue();
+                originalBright[currentPoint.X, currentPoint.Y] = brightness;
+                processedPoints.Add(currentPoint);
+                if (currentPoint.X - 2 < pictureBox1.Width &&
+                    originalBright[currentPoint.X - 2, currentPoint.Y] != origBrightness)
+                    currentPoint = currentPoint;
+                for (int i = -1; i <= 1; ++i)
+                    for (int j = -1; j <= 1; ++j)
+                    {
+                        if (i == 0 && j == 0) continue;
+                        nextPoint = new Point(currentPoint.X + i, currentPoint.Y + j);
+                        if (nextPoint.X > 0 && nextPoint.X < pictureBox1.Width &&
+                            nextPoint.Y > 0 && nextPoint.Y < pictureBox1.Height &&
+                            originalBright[nextPoint.X, nextPoint.Y] == origBrightness &&
+                            !processedPoints.Contains(nextPoint) && !nextPoints.Contains(nextPoint))
+                            nextPoints.Enqueue(nextPoint);
+                    }
+            }
+        }
+        #endregion
+    }
+}
